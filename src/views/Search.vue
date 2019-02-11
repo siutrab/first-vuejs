ecbee30bf9d3c2fdc2176ec748a5b867823916a0
siutrab <siutrab1@gmail.com>
2019-02-12 00:14:49 +0100

    Created basic skeleon of search layout

---
+++ src/views/Search.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Search:</label>
+      <input id="searh" type="text" name="search" v-model="searchValue" @input="inputHandler">
+    </div>
+  </div>
+</template>
+
+<script>
+//Api handling
+const API = "http://images-api.nasa.gov";
+
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchValue:
+        "" /*v-model jest obsługiwany poprzez funkcję zwracającą wartość v-modelu*/
+    };
+  },
+  methods: {
+    inputHandler() {
+      console.log(this.searchValue);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+}
+
+.search {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+}
+
+input {
+  height: 30px;
+  border-bottom: 1px solid;
+  border-bottom-color: cadetblue;
+}
+</style>
