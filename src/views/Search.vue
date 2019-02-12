b7005d9fa45469a03a884694d9766a2e045e0c8c
siutrab <siutrab1@gmail.com>
2019-02-12 01:24:14 +0100

    Connected to NASA API

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,25 +4,49 @@
       <label for="search">Search:</label>
       <input id="searh" type="text" name="search" v-model="searchValue" @input="inputHandler">
     </div>
+    <ul>
+      <li v-for="item in results" :key="item.data[0].nasa_id">
+        <p>{{item.data[0].description}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+//Axios is an request menager
+import axios from "axios";
+
+// debounce is an method from lodash bibliotec.
+// debounce func. activates some block of code after
+// there is no events noticed for given time
+import debounce from "lodash.debounce";
+
 //Api handling
-const API = "http://images-api.nasa.gov";
+const API = "http://images-api.nasa.gov/search";
 
 export default {
   name: "Search",
   data() {
     return {
       searchValue:
-        "" /*v-model jest obsługiwany poprzez funkcję zwracającą wartość v-modelu*/
+        "" /*v-model jest obsługiwany poprzez funkcję zwracającą wartość v-modelu*/,
+
+      results: []
     };
   },
+
   methods: {
-    inputHandler() {
-      console.log(this.searchValue);
-    }
+    inputHandler: debounce(function() {
+      axios
+        .get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then(response => {
+          this.results = response.data.collection.items;
+          console.log(this.results);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, 500)
   }
 };
 </script>
